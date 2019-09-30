7e5283bacf42848cc0401cba57753aa5ec813cca
abritopach <adrbrpa1988@gmail.com>
2019-09-30 10:32:50 +0200

    Switching to TypeScript: turn component controllers into classes

--- app/phone-list/phone-list.component.ts
+++ app/phone-list/phone-list.component.ts
@@ -1,14 +1,23 @@
-'use strict';
+class PhoneListController {
+  phones: any[];
+  orderProp: string;
+  query: string;
+
+  // The dependency injection annotations are attached to the class using a static property $inject. 
+  // At runtime this becomes the PhoneListController.$inject property.
+  static $inject = ['Phone'];
+  constructor(Phone: any) {
+    this.phones = Phone.query();
+    this.orderProp = 'age';
+  }
+
+}
+
 
 // Register `phoneList` component, along with its associated controller and template
 angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
-        this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
+    controller: PhoneListController
   });
--- dist/out-tsc/phone-list/phone-list.component.js
+++ dist/out-tsc/phone-list/phone-list.component.js
@@ -1,14 +1,18 @@
-'use strict';
+var PhoneListController = /** @class */ (function () {
+    function PhoneListController(Phone) {
+        this.phones = Phone.query();
+        this.orderProp = 'age';
+    }
+    // The dependency injection annotations are attached to the class using a static property $inject. 
+    // At runtime this becomes the PhoneListController.$inject property.
+    PhoneListController.$inject = ['Phone'];
+    return PhoneListController;
+}());
 // Register `phoneList` component, along with its associated controller and template
 angular.
     module('phoneList').
     component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-        function PhoneListController(Phone) {
-            this.phones = Phone.query();
-            this.orderProp = 'age';
-        }
-    ]
+    controller: PhoneListController
 });
 //# sourceMappingURL=phone-list.component.js.map
